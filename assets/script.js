5e50a10a3afe11bf499b6d60875f5467cc28e31b
dylangriess <dylangriess@gmail.com>
2022-08-13 22:57:26 -0600

    added past, present, future colors and classes to each hour block

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,23 @@
+//Display current date in Jumbotron
 var currentDate = moment().format("MMMM Do YYYY");
 $("#currentDay").text(currentDate);
+
+//format hour "H" to identify past, present, future hour block
+var hourBlock = $(".hour");
+// console.log(hourBlock);
+var timeNow = parseInt(moment().format("H"));
+// console.log(timeNow);
+
+//define each hour as past, present, future --> add class
+$.each(hourBlock, function (i, hour) {
+  var hourId = parseInt($(this).attr("id"));
+  console.log(i, hour);
+  console.log(hourId);
+  if (hourId === timeNow) {
+    $(this).next().addClass("present");
+  } else if (hourId < timeNow) {
+    $(this).next().addClass("past");
+  } else if (hourId > timeNow) {
+    $(this).next().addClass("future");
+  }
+});
